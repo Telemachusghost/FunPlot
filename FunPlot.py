19df96eb4616045650131a859dc5f4c62985f460
Telemachusghost <34588681+Telemachusghost@users.noreply.github.com>
2018-04-24 16:49:36 -0700

    Added scrolling to the canvas, intervals, fixed domains
    Added to argparser to kinda cheat/fix x**x and x**(1/2) x**0.5. This obviously does not really fix the issue we need to fix it by making the parser able to figure out the domain. I added scrolling to canvas which does not exactly work great but alongside the ability to choose interval adds some more functionality.

--- FunPlot.py
+++ FunPlot.py
@@ -22,11 +22,14 @@ and plots them using tkinter. Can understand standard python expressions.
 
 ************************
 
-author Derick Falk
+author Derick Falk, Daniel Denniston, Thomas Bowers
 """
 
+# Some important variables
 DEFAULT_HEIGHT = 500
 DEFAULT_WIDTH = 500
+
+
 	
 
 # The root plotting window
@@ -37,10 +40,20 @@ class graph(tk.Tk):
 		tk.Tk.__init__(self)
 		self.width = width
 		self.height = height
-		
 
-		self.can = Canvas(self, width = width, height = height, bg = 'white')
-		self.can.pack()
+		self.can = Canvas(self, width = width , height = height, bg = 'white')
+		
+		hbar=Scrollbar(self,orient=HORIZONTAL)
+		hbar.pack(side=BOTTOM,fill=X)
+		hbar.config(command=self.can.xview)
+		
+		vbar=Scrollbar(self,orient=VERTICAL)
+		vbar.pack(side=RIGHT,fill=Y)
+		vbar.config(command=self.can.yview)
+
+		
+		self.can.config(xscrollcommand=hbar.set, yscrollcommand=vbar.set, scrollregion=(0,0,2000,2000))
+		self.can.pack(side=LEFT,expand=True,fill=BOTH)
 		
 	# Method to draw the marks for the interval
 	def drawmarks(self, interval):
@@ -49,18 +62,19 @@ class graph(tk.Tk):
 		mark = self.width/s
 		for i in np.arange(0, self.width, mark):
 			self.can.create_line(i, self.height//2+10, i, self.height//2-10)
-			
+		
 
 		for i in np.arange(0, self.height, mark):
 			self.can.create_line(self.width//2+10, i, self.width//2-10, i)
 
+
 	# Method to draw the graph implement an arg parser at a later time *work
-	def drawgraph(self, expression=1, interval=(-10,10)):
+	def drawgraph(self, expression=1, interval=[-10,10]):
 		
 		s = interval[1] - interval[0]
 		mark = self.width//s
 		if mark < 10:
-			self.adjustcanvas()
+			self.adjustcanvas(s)
 
 		mark = self.width/s
 		self.can.delete('all')
@@ -71,21 +85,26 @@ class graph(tk.Tk):
 		self.drawmarks(interval)
 		ypoints = self.argparse(expression, interval)
 			
-		for x in np.arange(interval[0], interval[1], 0.001):
-			self.can.create_oval((self.width/2)+x*mark-1, (self.height/2)+-ypoints[y]*mark-1, (self.width/2)+x*mark+1, (self.height/2)+-ypoints[y]*mark+1 )
+		for x in np.arange(interval[0], interval[1], 0.01):
+			self.can.create_oval((self.width/2)+x*mark-2, (self.height/2)+-ypoints[y]*mark-2, (self.width/2)+x*mark+2, (self.height/2)+-ypoints[y]*mark+2 )
 			y += 1
 
 	# Adjusts canvas size
-	def adjustcanvas(self):
+	def adjustcanvas(self,interval):
 		self.width *= 1.5
 		self.height *= 1.5
-		self.can.config(width=self.width, height=self.height)
+		self.can.config(scrollregion=self.can.bbox('all'))
+		s = self.width//interval
+		if s < 10:
+			self.adjustcanvas(s)
 
 	# The arg parser for the string expression *work
 	def argparse(self, expression, interval):
 		ypts = [] 
+		if 'x**x' or 'x**0.5' in expression:
+			interval[0]=0
 		try:
-			for x in np.arange(interval[0], interval[1], 0.001):
+			for x in np.arange(interval[0], interval[1], 0.01):
 				ypts.append(eval(expression))
 			return ypts 
 		except:
@@ -93,7 +112,11 @@ class graph(tk.Tk):
 	
 		
 
-
+def intervalget():
+	interval = []
+	interval.append(int(intervalleft.get()))
+	interval.append(int(intervalright.get()))
+	return interval
 
 # Root window for graphing program
 app = graph()
@@ -126,7 +149,7 @@ e = Entry(expressionin, textvariable=expression)
 e.pack(side=LEFT)
 	
 	
-b = Button(expressionin, text="Plot", command = lambda: app.drawgraph(expression=e.get()))
+b = Button(expressionin, text="Plot", command = lambda: app.drawgraph(expression=e.get(), interval=intervalget()))
 b.pack(side = RIGHT)
 
 
@@ -138,7 +161,7 @@ intervalright.pack(side = RIGHT)
 
 # key bindings	
 app.bind('<Return>',lambda event: app.drawgraph(expression=e.get()))
-input_expression.bind('<Return>',lambda event: app.drawgraph(expression=e.get()))
+input_expression.bind('<Return>',lambda event: app.drawgraph(expression=e.get(), interval = intervalget()))
 
 # Main loop of program
 app.mainloop()
